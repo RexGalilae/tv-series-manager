32c45b9494743c0957a349df05d7f37e651dcdf9
RexGalilae <mdzaidgr8@gmail.com>
2018-06-12 06:59:08 +0400

    Add Remove Show + Other Minor Tweaks
    Add remove show and a few interactive elements to the menu

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from writer import *
 import platform
+import json
 
-platform.system()
 def new_show():
     print("Show :", end=" ")
     name = input()
@@ -37,13 +37,45 @@ def update():
     update_show(num)
     pak()
 
+# Remove Show
+def remove_show():
+    print_shows()
+    print("[D] - Delete All")
+    print("Option :", end = " ")
+    opt = input()
+    if not os.path.exists(DATAFILE):
+        print("There are no shows in your playlist.")
+    with open(DATAFILE, "r") as dfile:
+        shows = json.load(dfile)
+    if opt == 'D':
+         print("Confirm (Delete All) [Y/N]:"),
+         conf = input()
+         if conf == 'Y' or conf == 'y':
+             shows = []
+    else:
+        del shows[int(opt)]
+    with open(DATAFILE, "w") as dfile:
+        shows = json.dump(shows, dfile)
+    print("Show(s) successfully deleted!")
+    pak()
+
+def exit_program():
+    print("Thank You!")
+    pak()
+    clear_screen()
+    exit()
+
+def wip():
+    print("Sorry! This feature is unavailable right now. Stay updated!")
+    pak()
+
 options = {
     1 : ("New Show", new_show),
     2 : ("Update Show", update),
     3 : ("Show Playlist", show_playlist),
-    4 : ("Remove Show", exit),   #WIP
-    5 : ("Download Show (WIP)", exit), #WIP
-    0 : ("Exit", exit)
+    4 : ("Remove Show", remove_show),   #WIP
+    5 : ("Download Show (WIP)", wip), #WIP
+    0 : ("Exit", exit_program)
 }
 
 def clear_screen():
